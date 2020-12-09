3b6dc7f910aaa121b917666617d0ad1b70daef8e
Harold Reed <haroldreed@harolds-MacBook.local>
2020-12-09 10:10:25 -0500

    ok, working again; issue with update-counter resolved?

--- handlers/update-counter.js
+++ handlers/update-counter.js
@@ -1,24 +1,34 @@
-var AWS = require('aws-sdk');
-var docClient = new AWS.DynamoDB.DocumentClient();
+'use strict'
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 //const rp = require('minimal-require-promise')
 
+
 function updateCounter(siteId) {
-  
-//  console.log("Updating the item...");
-  
+
   return docClient.update({
     TableName: 'resume-counters',
     Key: {
       siteId: siteId
     },
     UpdateExpression: 'set resCounter = resCounter + :val',
+//    UpdateExpression: 'set resCounter = resCounter + 1',
     ExpressionAttributeValues: {
-      ':val': 1
+      ":val": 1
     },
     ReturnValues: 'ALL_NEW'
-  }) 
-//  }).promise()
-//  .then(result => result.Item)
+  }).promise()
+  .then((result) => {
+    console.log('Order is updated!', result)
+    return result.Attributes
+  })
+  .catch((updateError) => {
+    console.log(`Oops, order is not updated :(`, updateError)
+    throw updateError
+  })
+
+
 }
 
 module.exports =  updateCounter
